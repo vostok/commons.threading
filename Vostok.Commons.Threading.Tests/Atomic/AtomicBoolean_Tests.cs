a8d91ec1d3bda540b7ad6399b25240a6eb6bde76
razboynikov <razboynikov@skbkontur.ru>
2018-07-31 18:59:47 +0500

    Added all files.

---
+++ Vostok.Commons.Threading.Tests/Atomic/AtomicBoolean_Tests.cs
@@ -0,0 +1,66 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using Vostok.Commons.Threading.Atomic;
+
+namespace Vostok.Commons.Threading.Tests.Atomic
+{
+    [TestFixture]
+    public class AtomicBoolean_Tests
+    {
+        private AtomicBoolean boolean;
+
+        [SetUp]
+        public void TestSetup()
+        {
+            boolean = new AtomicBoolean(false);
+        }
+
+        [Test]
+        public void TrySetTrue_should_return_true_if_value_was_false()
+        {
+            boolean.TrySetTrue().Should().BeTrue();
+        }
+
+        [Test]
+        public void TrySetTrue_should_set_value_to_true()
+        {
+            boolean.TrySetTrue();
+
+            boolean.Value.Should().BeTrue();
+        }
+
+        [Test]
+        public void TrySetTrue_should_return_false_if_value_was_true()
+        {
+            boolean.TrySetTrue();
+
+            boolean.TrySetTrue().Should().BeFalse();
+        }
+
+        [Test]
+        public void TrySetFalse_should_return_true_if_value_was_true()
+        {
+            boolean.Value = true;
+
+            boolean.TrySetFalse().Should().BeTrue();
+        }
+
+        [Test]
+        public void TrySetFalse_should_set_value_to_false()
+        {
+            boolean.Value = true;
+
+            boolean.TrySetFalse();
+
+            boolean.Value.Should().BeFalse();
+        }
+
+        [Test]
+        public void TrySetFalse_should_return_false_if_value_was_false()
+        {
+            boolean.TrySetFalse();
+
+            boolean.TrySetFalse().Should().BeFalse();
+        }
+    }
+}
---
+++ Vostok.Commons.Threading/Atomic/AtomicBoolean.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading;
+
+namespace Vostok.Commons.Threading.Atomic
+{
+    public class AtomicBoolean
+    {
+        private const int TrueState = 1;
+        private const int FalseState = 0;
+
+        private int state;
+
+        public AtomicBoolean(bool initialValue) =>
+            state = initialValue ? TrueState : FalseState;
+
+        public bool Value
+        {
+            get => state == TrueState;
+            set => Interlocked.Exchange(ref state, value ? TrueState : FalseState);
+        }
+
+        public bool TrySetTrue() =>
+            Interlocked.CompareExchange(ref state, TrueState, FalseState) == FalseState;
+
+        public bool TrySetFalse() =>
+            Interlocked.CompareExchange(ref state, FalseState, TrueState) == TrueState;
+
+        public static implicit operator bool(AtomicBoolean atomicBoolean) =>
+            atomicBoolean.Value;
+    }
+}
