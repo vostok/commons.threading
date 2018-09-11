55b9ec0943284ec355a4fe16be224307035b2bc4
iloktionov <iloktionov@skbkontur.ru>
2018-09-11 19:44:15 +0500

    + LifoSemaphire

---
+++ Vostok.Commons.Threading.Tests/LifoSemaphore_Tests.cs
@@ -0,0 +1,179 @@
+﻿using FluentAssertions;
+using FluentAssertions.Extensions;
+using NUnit.Framework;
+
+namespace Vostok.Commons.Threading.Tests
+{
+    [TestFixture]
+    internal class LifoSemaphore_Tests
+    {
+        private LifoSemaphore semaphore;
+
+        [SetUp]
+        public void TestSetup()
+        {
+            semaphore = new LifoSemaphore(3);
+        }
+
+        [Test]
+        public void WaitAsync_should_return_completed_tasks_immediately_until_count_goes_negative()
+        {
+            semaphore.WaitAsync().IsCompleted.Should().BeTrue();
+            semaphore.WaitAsync().IsCompleted.Should().BeTrue();
+            semaphore.WaitAsync().IsCompleted.Should().BeTrue();
+        }
+
+        [Test]
+        public void WaitAsync_should_return_waiter_tasks_after_count_goes_negative()
+        {
+            semaphore.WaitAsync();
+            semaphore.WaitAsync();
+            semaphore.WaitAsync();
+
+            semaphore.WaitAsync().IsCompleted.Should().BeFalse();
+            semaphore.WaitAsync().IsCompleted.Should().BeFalse();
+        }
+
+        [Test]
+        public void CurrentCount_property_should_initially_return_initial_count()
+        {
+            semaphore.CurrentCount.Should().Be(3);
+        }
+
+        [Test]
+        public void CurrentCount_property_should_decrease_after_wait_calls()
+        {
+            for (var i = 1; i <= 3; i++)
+            {
+                semaphore.WaitAsync();
+
+                semaphore.CurrentCount.Should().Be(3 - i);
+            }
+        }
+
+        [Test]
+        public void CurrentCount_property_should_return_zero_when_there_is_a_queue_of_waiters()
+        {
+            for (var i = 1; i <= 5; i++)
+            {
+                semaphore.WaitAsync();
+            }
+
+            semaphore.CurrentCount.Should().Be(0);
+        }
+
+        [Test]
+        public void CurrentQueue_property_should_initially_return_zero()
+        {
+            semaphore.CurrentQueue.Should().Be(0);
+        }
+
+        [Test]
+        public void CurrentQueue_property_should_remain_zero_until_there_is_first_queued_waiter()
+        {
+            for (var i = 0; i < 3; i++)
+            {
+                semaphore.WaitAsync();
+                semaphore.CurrentQueue.Should().Be(0);
+            }
+        }
+
+        [Test]
+        public void CurrentQueue_property_should_return_count_of_queued_waiters_if_there_are_any()
+        {
+            for (var i = 0; i < 3; i++)
+            {
+                semaphore.WaitAsync();
+            }
+
+            for (var i = 1; i <= 3; i++)
+            {
+                semaphore.WaitAsync();
+                semaphore.CurrentQueue.Should().Be(i);
+            }
+        }
+
+        [Test]
+        public void Release_should_increase_current_count_by_one()
+        {
+            for (var i = 1; i <= 3; i++)
+            {
+                semaphore.Release();
+                semaphore.CurrentCount.Should().Be(3 + i);
+            }
+        }
+
+        [Test]
+        public void Release_should_unblock_pending_waiter()
+        {
+            for (var i = 0; i < 3; i++)
+                semaphore.WaitAsync();
+
+            var waiter = semaphore.WaitAsync();
+
+            semaphore.Release();
+
+            waiter.Wait(5.Seconds()).Should().BeTrue();
+        }
+
+        [Test]
+        public void Release_should_unblock_waiters_in_lifo_order()
+        {
+            for (var i = 0; i < 3; i++)
+                semaphore.WaitAsync();
+
+            var waiter1 = semaphore.WaitAsync();
+            var waiter2 = semaphore.WaitAsync();
+            var waiter3 = semaphore.WaitAsync();
+
+            semaphore.Release();
+
+            waiter3.Wait(5.Seconds()).Should().BeTrue();
+            waiter2.IsCompleted.Should().BeFalse();
+            waiter1.IsCompleted.Should().BeFalse();
+
+            semaphore.Release();
+
+            waiter2.Wait(5.Seconds()).Should().BeTrue();
+            waiter1.IsCompleted.Should().BeFalse();
+
+            semaphore.Release();
+
+            waiter1.Wait(5.Seconds()).Should().BeTrue();
+        }
+
+        [Test]
+        public void Release_with_count_should_increase_current_count_by_given_amount()
+        {
+            semaphore.Release(3);
+            semaphore.CurrentCount.Should().Be(6);
+        }
+
+        [Test]
+        public void Release_with_count_should_unblock_pending_waiters_in_lifo_order()
+        {
+            for (var i = 0; i < 3; i++)
+                semaphore.WaitAsync();
+
+            var waiter1 = semaphore.WaitAsync();
+            var waiter2 = semaphore.WaitAsync();
+            var waiter3 = semaphore.WaitAsync();
+
+            semaphore.Release(2);
+
+            waiter3.Wait(5.Seconds()).Should().BeTrue();
+            waiter2.Wait(5.Seconds()).Should().BeTrue();
+            waiter1.IsCompleted.Should().BeFalse();
+
+            semaphore.CurrentCount.Should().Be(0);
+            semaphore.CurrentQueue.Should().Be(1);
+
+            semaphore.Release(2);
+
+            waiter1.Wait(5.Seconds()).Should().BeTrue();
+
+            semaphore.CurrentCount.Should().Be(1);
+            semaphore.CurrentQueue.Should().Be(0);
+        }
+    }
+}
---
+++ Vostok.Commons.Threading.Tests/LifoSemaphore_Tests_Performance.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Toolchains.InProcess;
+using NUnit.Framework;
+
+namespace Vostok.Commons.Threading.Tests
+{
+    [TestFixture]
+    [Explicit]
+    internal class LifoSemaphore_Tests_Performance
+    {
+        [SetUp]
+        public void TestSetup()
+        {
+            ThreadPool.SetMinThreads(2048, 2048);
+        }
+
+        [Test]
+        [Explicit]
+        public void Benchmark_single_threaded_speed_vs_SemaphoreSlim()
+        {
+            BenchmarkRunnerCore.Run(BenchmarkConverter.TypeToBenchmarks(typeof(LifoSemaphoreBenchmark)),
+                job => new InProcessToolchain(false));
+        }
+
+        [TestCase(1, 1, 500 * 1000)]
+        [TestCase(1, 2, 75 * 1000)]
+        [TestCase(1, 4, 50 * 1000)]
+        [TestCase(1, 8, 30 * 1000)]
+        [TestCase(1, 16, 15 * 1000)]
+        [TestCase(1, 32, 10 * 1000)]
+        [TestCase(2, 2, 250 * 1000)]
+        [TestCase(2, 3, 45 * 1000)]
+        [TestCase(2, 4, 40 * 1000)]
+        [TestCase(2, 8, 25 * 1000)]
+        [TestCase(2, 16, 12 * 1000)]
+        [TestCase(2, 32, 10 * 1000)]
+        [TestCase(3, 3, 45 * 1000)]
+        [TestCase(3, 4, 40 * 1000)]
+        [TestCase(3, 8, 25 * 1000)]
+        [TestCase(3, 16, 12 * 1000)]
+        [TestCase(3, 32, 10 * 1000)]
+        [TestCase(4, 4, 40 * 1000)]
+        [TestCase(4, 5, 25 * 1000)]
+        [TestCase(4, 8, 20 * 1000)]
+        [TestCase(4, 16, 12 * 1000)]
+        [TestCase(4, 32, 10 * 1000)]
+        [TestCase(4, 64, 3 * 1000)]
+        [TestCase(8, 8, 250 * 1000)]
+        [TestCase(8, 16, 25 * 1000)]
+        [TestCase(8, 32, 20 * 1000)]
+        [TestCase(8, 64, 3 * 1000)]
+        [TestCase(8, 128, 3 * 1000)]
+        [TestCase(16, 16, 250 * 1000)]
+        [TestCase(16, 20, 50 * 1000)]
+        [TestCase(16, 32, 40 * 1000)]
+        [TestCase(16, 64, 15 * 1000)]
+        [TestCase(16, 128, 10 * 1000)]
+        [TestCase(32, 32, 250 * 1000)]
+        [TestCase(32, 45, 150 * 1000)]
+        [TestCase(32, 64, 75 * 1000)]
+        [TestCase(32, 128, 25 * 1000)]
+        [TestCase(32, 256, 7 * 1000)]
+        [TestCase(64, 64, 75 * 1000)]
+        [TestCase(64, 65, 75 * 1000)]
+        [TestCase(64, 100, 45 * 1000)]
+        [TestCase(64, 128, 20 * 1000)]
+        [TestCase(64, 256, 10 * 1000)]
+        [TestCase(250, 32, 100 * 1000)]
+        [TestCase(250, 64, 100 * 1000)]
+        [TestCase(250, 128, 75 * 1000)]
+        [TestCase(250, 192, 75 * 1000)]
+        [TestCase(250, 249, 50 * 1000)]
+        [TestCase(250, 251, 40 * 1000)]
+        [TestCase(250, 260, 40 * 1000)]
+        [TestCase(250, 300, 25 * 1000)]
+        [TestCase(250, 1001, 5 * 1000)]
+        [Explicit]
+        public void Benchmark_multi_threaded_speed_vs_SemaphoreSlim(int capacity, int parallelism, int iterations)
+        {
+            var slimTimes = new List<double>();
+            var lifoTimes = new List<double>();
+
+            var slimSemaphore = new SemaphoreSlim(capacity, capacity);
+            var lifoSemaphore = new LifoSemaphore(capacity);
+
+            for (var i = 0; i < 5; i++)
+            {
+                var watch = Stopwatch.StartNew();
+
+                var tasks = new List<Task>();
+
+                for (var j = 0; j < parallelism; j++)
+                {
+                    tasks.Add(Task.Run(async () =>
+                    {
+                        for (var k = 0; k < iterations; k++)
+                        {
+                            await slimSemaphore.WaitAsync();
+
+                            slimSemaphore.Release();
+                        }
+                    }));
+                }
+
+                Task.WhenAll(tasks).GetAwaiter().GetResult();
+
+                slimTimes.Add(watch.Elapsed.TotalMilliseconds);
+
+                Console.Out.WriteLine($"Slim: {watch.Elapsed.TotalMilliseconds:F2} ms");
+            }
+
+            var avgSlimTime = slimTimes.Average();
+
+            Console.Out.WriteLine();
+            Console.Out.WriteLine($"Slim avg: {avgSlimTime:F2} ms");
+            Console.Out.WriteLine();
+
+            for (var i = 0; i < 5; i++)
+            {
+                var watch = Stopwatch.StartNew();
+
+                var tasks = new List<Task>();
+
+                for (var j = 0; j < parallelism; j++)
+                {
+                    tasks.Add(Task.Run(async () =>
+                    {
+                        for (var k = 0; k < iterations; k++)
+                        {
+                            await lifoSemaphore.WaitAsync();
+
+                            lifoSemaphore.Release();
+                        }
+                    }));
+                }
+
+                Task.WhenAll(tasks).GetAwaiter().GetResult();
+
+                lifoTimes.Add(watch.Elapsed.TotalMilliseconds);
+
+                Console.Out.WriteLine($"Lifo: {watch.Elapsed.TotalMilliseconds:F2} ms");
+            }
+
+            var avgLifoTime = lifoTimes.Average();
+
+            Console.Out.WriteLine();
+            Console.Out.WriteLine($"Lifo avg: {avgLifoTime:F2} ms");
+            Console.Out.WriteLine();
+
+            if (avgLifoTime < avgSlimTime)
+            {
+                Console.Out.WriteLine($"Winner is LifoSemaphore ({avgSlimTime / avgLifoTime:F2}x)!");
+                Assert.Pass();
+            }
+            else
+            {
+                Console.Out.WriteLine($"Winner is SemaphoreSlim ({avgLifoTime / avgSlimTime:F2}x)!");
+                Assert.Inconclusive();
+            }
+        }
+    }
+}
