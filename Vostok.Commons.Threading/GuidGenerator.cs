d654a3fb1aa72c6f6eeaf7a2e378ae167d1016e9
deniaa <deniaa@skbkontur.ru>
2021-07-02 14:43:30 +0500

    Place GuidGenerator in this module next to ThreadSafeRandom.

---
+++ Vostok.Commons.Threading/GuidGenerator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using JetBrains.Annotations;
+
+namespace Vostok.Commons.Threading
+{
+    [PublicAPI]
+    internal static class GuidGenerator
+    {
+        public static unsafe Guid GenerateNotCryptoQualityGuid()
+        {
+            var bytes = stackalloc byte[16];
+            var dst = bytes;
+
+            var random = ThreadSafeRandom.ObtainThreadStaticRandom();
+            for (var i = 0; i < 4; i++)
+            {
+                *(int*)dst = random.Next();
+                dst += 4;
+            }
+
+            return *(Guid*)bytes;
+        }
+    }
+}
